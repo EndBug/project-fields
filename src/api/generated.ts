0ca5c4ffbfee518992a54b7b5ac2fff4a2657211
Federico Grandi <fgrandi30@gmail.com>
2024-04-03 10:50:14 +0200

    chore: run linter

--- src/api/generated.ts
+++ src/api/generated.ts
@@ -53,43 +53,43 @@ export type RequestParams<T extends Request> = T extends 'getField'
       fieldName: string;
     }
   : T extends 'getFieldValues'
-  ? {
-      resourceUrl: string;
-    }
-  : T extends 'getItemId'
-  ? {
-      resourceUrl: string;
-    }
-  : T extends 'getProjectId'
-  ? {
-      owner: string;
-      number: number;
-    }
-  : T extends 'clearItemFieldValue'
-  ? {
-      projectId: string;
-      itemId: string;
-      fieldId: string;
-    }
-  : T extends 'setItemFieldValue'
-  ? {
-      projectId: string;
-      itemId: string;
-      fieldId: string;
-      value: ProjectV2FieldValue;
-    }
-  : never;
+    ? {
+        resourceUrl: string;
+      }
+    : T extends 'getItemId'
+      ? {
+          resourceUrl: string;
+        }
+      : T extends 'getProjectId'
+        ? {
+            owner: string;
+            number: number;
+          }
+        : T extends 'clearItemFieldValue'
+          ? {
+              projectId: string;
+              itemId: string;
+              fieldId: string;
+            }
+          : T extends 'setItemFieldValue'
+            ? {
+                projectId: string;
+                itemId: string;
+                fieldId: string;
+                value: ProjectV2FieldValue;
+              }
+            : never;
 
 export type Response<T extends Request> = T extends 'getField'
   ? ResponseTypes.getField
   : T extends 'getFieldValues'
-  ? ResponseTypes.getFieldValues
-  : T extends 'getItemId'
-  ? ResponseTypes.getItemId
-  : T extends 'getProjectId'
-  ? ResponseTypes.getProjectId
-  : T extends 'clearItemFieldValue'
-  ? ResponseTypes.clearItemFieldValue
-  : T extends 'setItemFieldValue'
-  ? ResponseTypes.setItemFieldValue
-  : never;
+    ? ResponseTypes.getFieldValues
+    : T extends 'getItemId'
+      ? ResponseTypes.getItemId
+      : T extends 'getProjectId'
+        ? ResponseTypes.getProjectId
+        : T extends 'clearItemFieldValue'
+          ? ResponseTypes.clearItemFieldValue
+          : T extends 'setItemFieldValue'
+            ? ResponseTypes.setItemFieldValue
+            : never;
